4efb7633dc13d346e7d58295aea78d98a47459b1
Scyonix <scyonix17@gmail.com>
2023-04-19 17:56:57 +0500

    saving theme

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,1 +1,28 @@
 // Add your inline JS here
+
+// Saving Theme in local storage //
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+const storeTheme = function(theme){
+    localStorage.setItem("theme", theme);
+};
+
+// Retrieving Theme from local storage //
+const retrieveTheme = function() {
+    const activeTheme = localStorage.getItem("theme");
+
+    colorThemes.forEach((themeOption) => {
+        if(themeOption.id === activeTheme) {
+            themeOption.checked = true;
+        }
+    });
+};
+
+colorThemes.forEach( (themeOption)  => {
+    _themeColor.addEventListener("click",()=>{
+        storeTheme(themeOption.id);
+    });
+});
+
+document.onload = retrieveTheme();
+
